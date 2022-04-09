c417fd94de6be9733f8ba776e521eb30e5bec249
Alexandru Bancila <sasa.bancila@gmail.com>
2022-04-09 23:38:22 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+// @ts-ignore
+import item from '../app/new things (1).json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'untitled';
+  title = 'json-file-read-angular';
+  public itemList: { id: number, areaId: number, joinedWith: number, sku: string, defaultSku: string, status: string, countActive: number }[] = item;
 }
